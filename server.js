962c6afe32366b26cd0635b924d13b09b28722d0
Michael Wilburn <michael-mw@hotmail.com>
2026-02-26 16:04:00 -0300

    Responding with data

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
-const http = require('http')
+const http = require('http');
+
+const todos = [
+  { id: 1, text: 'Todo One' },
+  { id: 2, text: 'Todo Two' },
+  { id: 3, text: 'Todo Three' }
+];
 
 const server = http.createServer((req, res) => {
-    const { method, url, headers, body } = req;
-    console.log(method, url, headers);
-    console.log(body);
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('X-Powered-By', 'Node.js');
 
-    res.end()
+  res.end(JSON.stringify({
+    success: true,
+    data: todos
+  }));
 });
 
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => {console.log(`Server started on port ${PORT}`)})
+server.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
